1e852a699915feec8bbb1aad5d89f6e9620ec908
nwabuike <nwabuikeanthony@gmail.com>
2021-07-13 14:07:17 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css" media="screen,projection" />
+    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/sweetalert2@10.10.1/dist/sweetalert2.min.css'>
+    <link type="text/css" rel="stylesheet" href="css/style.css" />
+    <title>Prep50 | Booklist</title>
+</head>
+
+<body>
+    <nav class="white" role="navigation">
+        <div class="nav-wrapper container">
+            <a id="logo-container" href="#" class="brand-logo red-text text-lighten-2">Prep50</a>
+        </div>
+    </nav>
+    <div class="carousel carousel-slider center" data-indicators="true">
+        <div class="carousel-fixed-item center">
+            <h5 class="white-text">Smash Your Jamb With Prep50 Past Questions</h5>
+        </div>
+        <div class="carousel-item white-text" href="#one!">
+            <!-- <h2>Prep50 Books</h2> -->
+            <img src="./img/back_3.jpg" class="responsive-img">
+            <!-- <p class="white-text">This is your Where Your success is Assured</p> -->
+        </div>
+        <div class="carousel-item white-text" href="#two!">
+            <!-- <h2>Prep50 Books</h2> -->
+            <img src="./img/waec_ad.jpg" class="responsive-img">
+            <!-- <p class="white-text">This is your Where Your success is Assured</p> -->
+        </div>
+        <div class="carousel-item red white-text" href="#four!">
+            <!-- <h2>Prep50 Books</h2> -->
+            <!-- <p class="white-text">This is your Where Your success is Assured</p> -->
+            <img src="./img/back_4.jpg" class="responsive-img">
+        </div>
+    </div>
+    <br>
+    <div class="container">
+        <div class="section">
+            <h5>Now that you are here, it really means you have genuine intention for your students. I need you to listen to this experiment and you can do one in your school after this.</h5>
+
+            <p>Go to any class from SS1 to SS 3, select any topic they have been taught this term and then go to any WAEC or JAMB past question around you and dig up the standard questions from that topic.(of course if you are not using Prep50, that will be hard for you to do, but try). If you are using Prep50, the questions are already arranged topic by topic according to the syllabus. So select 5 random questions and give the students a test/quiz. What we learnt from this experiment was unbelievable.</p>
+
+            <p>We did our own quiz with students immediately after the lecture was taught. We assumed that so long as the questions came immediately after the lessons that the students will perform better. No they didn't. Only a little above 10% of the students answered all the questions correctly. The other 89% failed and it was not because the test was sudden, No its because they couldn't even attempt the question.</p>
+
+            <p>We discovered that just as there is a serious degradation of values and standards in the society, there is also serious a watering-down of the whole education system even in Educationally Advantaged states. We have seen the continued drop down of cut-off marks and the results of the 2021 JAMB UTME that was released recently clearly buttresses this fact. Federal Universities have moved from 220 cut-off mark to 180/160 in the last 6 years. This year, because of the massive failure in JAMB, they will move further down to maybe 140 and State universities will go down to 120. Maybe it will be 60 for colleges of Education. This watering down is not sudden, it is well planned out in how we have prepared for lessons, taught them and evaluated our students in our schools; and the earlier we take responsibility the better.</p>
+
+            <p>Some of these teachers prepare lessons that are so sub-standard that after teaching, their students cannot answer standard WAEC and JAMB questions drawn from those topics. You can do the experiment yourself in your school. Don't get me wrong, it is good to bring down concepts and explain them in a way the students will understand, but it is also important to draw them up to the expected academic objectives for the lesson with the attendant critical thinking needed to tackle questions.</p>
+
+            <p>For example, you taught them separation techniques and the elements that can be separated using each technique and the question demands that they separate not just elements but mixtures and which must be done in a certain order. The lessons by the teacher were not enough to help students tackle the questions. We see it in every subject, a teacher will teach Geometric Progression in mathematics but will not teach the students, that some questions on this topic can lead to simultaneous equation.</p>
+
+            <p>See, when we tried to produce the prep50 mobile app, we brought-in the best teachers around to help us produce the notes on all the topics for selected subjects. They wrote the notes but we had to return them because the simple quality assurance test we devised was to try to see if we can answer all WAEC and JAMB questions on that topic after reading their notes. Only the winner of the Maltina Teacher Award and one other teacher passed the quality assurance test. When we asked them what they did differently, they confirmed that even without telling them, they have already learned to consult these standard past questions before preparing for any lesson.</p>
+
+            <h4> Do your teachers do so?</h4>
+
+            <p>Do they consult all past questions from WAEC and JAMB on the topic they are about to teach before preparing the lessons? I guess no, very many teachers don't and its so sad how these students are not well prepared for these exams and we all have resorted to exam malpractice to make it up. If your teachers are in the league of those who do, then this Prep50 series is not for you. One of the reasons the teachers we worked with gave for not going through the past questions before writing on any topic was that digging out these questions from the usual past questions arranged in years and categorizing them topic by topic and sub-topic by sub-topic is hard. That was how PREP50 was born. We went to work to make it easier for them and for you to see these questions topic by topic, sub-topic by subtopic and to make the most effective use of them.</p>
+
+            <p>This is the problem the PREP50 series has helped teachers, students and schools to solve and as such PREP50 must be included on your BOOKLIST for 2020/21 academic year. Here are the reasons why.</p>
+            <ul>
+                <li> 1. Every teacher that prepares any lesson without first going through all the past questions on that topic is doing a great disservice to our children. As principals and proprietors, we should not allow that to happen hence forth. We want students to benefit from this hindsight and also for the teachers to benefit by having a pool of standard questions from where they can draw their termly questions.</li>
+
+                <li>2. We are not asking you to make a heavy investment. Your school already procures workbooks and past questions for SSCE and JAMB for students, we are only asking you to adopt and recommend this more effective tool for your students but begin on time in SS1 rather than SS3. Our series are also structured class by class for easy access to topics especially for those in SS1 or SS2.</li>
+            </ul>
+
+            <p>The prep50 series captures all the topics in the SSCE and JAMB syllabus such that an SS1 student doing a particular topic in class can see how questions from that topic are framed in WAEC or JAMB. Many of you do approve and recommend workbooks on your booklist but when you open such workbooks, you will immediately see how watered-down they are, yet teachers go on to recommend them all because of commission.</p>
+
+            <p>Like, is it not so intuitive that when you want to produce a workbook for a particular subject, that you first of all scout and gather all manner of exam past questions on the subject, be it WAEC, NECO, JAMB, GCE anything exam body u can find. Does one need any supernatural powers to know that this is the only way one can design a better workbook, yet every workbook you open after another is lacking those standard questions from notable exam bodies. It just goes to show you how incompetent we have become in handling our schools and our children in this country. Well as we have learnt, the publishers are traders in the market and they probably care less about standard. Everything for them is margin and profit or maybe there is no alternative for teachers to rely on. Either way, we have produced a more viable alternative in PREP50.</p>
+
+            <p>Get Prep50 SSCE and JAMB for your teachers to ensure that every lesson they prepare meets the standard WAEC and JAMB test questions. Prep50 will make their loads and burden easier when they want set questions for test or exams</p>
+
+
+            <p>Get PREP50 for your students so that they can identify tough questions early enough and seek assistance or clarification from their teachers whenever the topic is being handled in class. This will end the complaint that teachers teach easy questions and evaluate students with tough ones. I understand that we do not have to spoon feed these students but I also know that majority of the teachers are not willing to put in the hard work or do any research themselves.</p>
+
+            <p>Add PREP50 to your book-list for all senior classes starting with SS1 and watch your students' exam performance skyrocket. This is not a question of turning blind eye when one or two of your teachers connive with the students to cheat in the examination and you are happy with the result. If you are like that, you are perpetuating the decline of this nation and nurturing young ones should not be your assignment. Take the right tools and start these preparations on time and watch your students get attuned to these exams. On the examination day, it will be more like a normal activity. Words like expatiate or outline will not be scary again.</p>
+
+            <p>Remeber, If you do not place that call to add PREP50 to your booklist now, you are like the vendor that allowed a pharmacist to sell chalk to people instead of paracetamol all for profit. The profit and commission mongers will approve their own and tell you to leave it next year as if they ever planned to consider it.</p>
+
+            <p>Tear that book-list NOW and redo another one.</p>
+
+            <p>Ensure that the best workbook ever which is Prep50 is recommended for SS1, SS2 and SS3 classes. Do it NOW and then give us a call for a favourable quotation for the books.</p>
+
+            <p>usually we sell in bundles to parents as you can see from the pix below. We have the Science bundles, Arts and Social Sciences for WAEC then Science & Arts bundle for JAMB.</p>
+
+            <p>Each waec bundle sells at N15,000 per bundle</p>
+
+            <p>while JAMB is N10,000.</p>
+
+            <p>But for our book-list promotion, we will be giving the schools a 40% discount when they buy in bulk.</p>
+
+            <p>SO for WAEC bundles, you will get it @ N9,000 (7 to 8 books in a bundle),</p>
+
+            <p>while the JAMB is N6000 per bundle (4 books in a bundle).</p>
+
+            <p>If you are buying both, you pay N15,000 instead of the usual N22,500. So technically, a school can procure JAMB and WAEC series at the price (N15,000) we sell the WAEC only to parents. Grab your copies NOW</p>
+
+            <p>We have included here some pictures and samples of our books, you can download them to have a feel of what you will get. If you are a parent reading this, send this link <a target="_blank" href="prep50booklist.prep50mobileapp.com.ng">prep50booklist.prep50mobileapp.com.ng</a> to your childs' teacher or any school authority you know. This madness must stop and you need to help.</p>
+
+            <a target="_blank" href="doc/Prep50 Chemistry Sample pages.pdf"><button class="btn btn-primary"><i class="material-icons">cloud_download</i> Download Chemistry</button></a>
+            <a target="_blank" href="doc/Prep50 Economics sample pages.pdf"><button class="btn btn-primary"><i class="material-icons">cloud_download</i> Download Econimics</button></a>
+            <a target="_blank" href="doc/Prep50 Maths Sample pages.pdf"><button class="btn btn-primary"><i class="material-icons">cloud_download</i> Download Maths</button></a>
+
+            <p>Answers can be found at the back of the book</p>
+
+            <p> Fill the form below, so we can give you a quotation based on the number of students in your senior secondary. Please do not fill the form if you are still doing book politics, we are not interested in that.</p>
+        </div>
+    </div>
+    <footer class="page-footer">
+
+        <div class="footer-copyright">
+            <div class="container">
+                © 2021 Copyright Prep50Books. Powered by Daecons Educational consult & Data center and Deacons Digital Solutions ltd.
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
